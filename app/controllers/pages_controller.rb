88fc8879a34a2c86d698aecfb469742b933e9df0
Aryana Pezé <aryana.ravari@gmail.com>
2017-01-23 15:11:07 +0100

    page structures ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,11 +4,13 @@ class PagesController < ApplicationController
   def home
     @prices = policy_scope(Price).order(name: :desc)
     user = User.where(email: "niloufer@ravari.fr")
-    @admin = user[0]
     @events = Event.where(home: true)
+    @admin = user[0]
   end
 
   def contact
+    user = User.where(email: "niloufer@ravari.fr")
+    @admin = user[0]
   end
 
 end
